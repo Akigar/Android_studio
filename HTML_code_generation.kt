97b9a7ab9ec54ef52db7e40dd812298963715312
Никита Гордеев <93983145+Akigar@users.noreply.github.com>
2022-12-04 23:06:56 +0800

    Create HTML_code_generation.kt

---
+++ HTML_code_generation.kt
@@ -0,0 +1,48 @@
+package com.example.message_mail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+data class Message(val address: String?, val topic: String?, val message: String?, val content: String?) {
+    //fun toHTML(): String = "$address ${topic}"
+}
+
+fun main() {
+    fun Message.toHTML(): String {
+        val style = "<head>\n" +
+                "\t<style>\n" +
+                "\t\tbody { \n" +
+                "\t\t\tbackground: rgb(85, 39, 180);\n" +
+                "font-size: 14px;\n" +
+                "\t\t\tfont-family: 'Arial' ;\n" +
+                "\t\t}\n" +
+                "\t</style>\n" +
+                "</head>\n" +
+                "\n<body>\n"
+        var template = style + "<table> " +
+                (address?.let { "\n\t<tr>\n\t\t<td>address</td>\n\t\t<td>$it</td>\n\t</tr>" }
+                    ?: "\n\t<tr>\n\t\t<td> </td>\n\t</tr>") +
+                (topic?.let { "\n\t<tr>\n\t\t<td>topic</td>\n\t\t<td>$it</td>\n\t</tr>" }
+                    ?: "\n\t<tr>\n\t\t<td> </td>\n\t</tr>") +
+                (message?.let { "\n\t<tr>\n\t\t<td>message</td>\n\t\t<td>$it</td>\n\t</tr>" }
+                    ?: "\n\t<tr>\n\t\t<td> </td>\n\t</tr>") +
+                (content?.let { "\n\t<tr>\n\t\t<td>content</td>\n\t\t<td>$it</td>\n\t</tr>" }
+                    ?: "\n\t<tr>\n\t\t<td> </td>\n\t</tr>") +
+                "\n</table>" + "\n</body>"
+        return template
+    }
+
+    val m = Message("nikita.gordeew2015@yandex.com", null, null, null)
+    val m1 = Message("nikita.gordeew2015@yandex.com", null, "nikita.gordeew2015@yandex.com", "Hello")
+    val m2 = Message("nikita.gordeew2015@yandex.com", "sometopic", "nikita.gordeew2015@yandex.com", "Hello")
+    println(m.toHTML())
+    println(m1.toHTML())
+    println(m2.toHTML())
+}
